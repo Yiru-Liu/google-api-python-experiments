6f6ff9bbb64bbe332b343a29066bbfee94400062
Yiru Liu <yiru.liu05@gmail.com>
2022-08-26 10:34:07 -0400

    Added a few test files

---
+++ list_calendars.py
@@ -0,0 +1,23 @@
+from ast import main
+from cal_setup import get_cal_service
+
+
+def main():
+    service = get_cal_service()
+    print("Getting list of calendars")
+    calendars_result = service.calendarList().list().execute()
+
+    calendars = calendars_result.get('items', [])
+
+    if not calendars:
+        print('No calendars found.')
+    for calendar in calendars:
+        summary = calendar['summary']
+        id = calendar['id']
+        primary = "Primary" if calendar.get('primary') else ""
+        print("%s\t%s\t%s" % (summary, id, primary))
+
+
+if __name__ == "__main__":
+    main()
+    
